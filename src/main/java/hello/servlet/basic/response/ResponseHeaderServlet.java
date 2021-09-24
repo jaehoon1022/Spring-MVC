f0d5668a87c176179f6b8272b3553d18ba2b252b
jaehoon1022 <hyunsense1022@gmail.com>
2021-09-24 11:55:18 +0900

    HttpServletResponse-기본 사용법

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -32,7 +32,7 @@ public class ResponseHeaderServlet extends HttpServlet {
         //[Redirect 편의 메서드]
 //        redirect(response);
 
-
+        //[message body]
         PrintWriter writer = response.getWriter();
         writer.println("ok");
     }
